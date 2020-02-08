76acb8ed9c14806894791de62ef39fe9abd00ce6
jialuzhang <jialuzhang0805@gmail.com>
2020-02-08 21:00:51 +0800

    管理输出

--- fellow_guanwang/src/index.js
+++ fellow_guanwang/src/index.js
@@ -1,11 +1,18 @@
 import _ from 'loadsh';
+import printMe from './print'
 
 function component() {
     var element = document.createElement('div');
-  
+    var btn = document.createElement('button');
+
     // Loadsh 现在通过import导入
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+    
     return element;
 }
   
