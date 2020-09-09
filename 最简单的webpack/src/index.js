55a2be4f0cbb105c8fe983a15a4f173bcbc622ba
tk_zjl <016760@zjtlcb.com>
2020-09-09 17:52:23 +0800

    添加热更新

--- 最简单的webpack/src/index.js
+++ 最简单的webpack/src/index.js
@@ -1,3 +1,8 @@
 import helloworld from './helloworld'
 
 document.write(helloworld())
+
+
+// if (module.hot) {
+//     module.hot.accept()
+// }
--- 最简单的webpack/webpack.config.js
+++ 最简单的webpack/webpack.config.js
@@ -1,62 +1,72 @@
 'use strict'
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
     entry: {
-        index:'./src/index.js',
-        search:'./src/search.js'
+        index: './src/index.js',
+        search: './src/search.js'
     },
-    output:{
-        path: path.join(__dirname,'dist'),
+    output: {
+        path: path.join(__dirname, 'dist'),
         filename: '[name].js'
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.js$/,
-                use:'babel-loader'
+                test: /\.js$/,
+                use: 'babel-loader'
             },
             {
-                test:/.css$/,
-                use:[
+                test: /.css$/,
+                use: [
                     "style-loader",
                     "css-loader"
                 ]
             },
             {
-                test:/.less$/,
-                use:[
+                test: /.less$/,
+                use: [
                     "style-loader",
                     "css-loader",
                     "less-loader"
                 ]
             },
             {
-                test:/.(jpg|png|jpeg|gif|svg)$/,
-                use:[
+                test: /.(jpg|png|jpeg|gif|svg)$/,
+                use: [
                     {
-                        loader:'url-loader',
-                        options:{
-                            limit:102400  //100k
+                        loader: 'url-loader',
+                        options: {
+                            limit: 102400  //100k
                         }
                     }
                 ]
             },
             {
-                test:/.(otf|tff)$/,
-                use:"file-loader"
+                test: /.(otf|tff)$/,
+                use: "file-loader"
             }
         ]
     },
-    watch:true,
+    watch: true,
     // 只有开启监听模式，watchOptions才有意义
-    watchOptions:{
+    watchOptions: {
         // 默认为空，不监听的文件或文件夹，支持正则表达
-        ignored:/node_modules/,
+        ignored: /node_modules/,
         // 监听到变化后等待300ms再去执行，默认300ms
-        aggregateTimeout:300,
+        aggregateTimeout: 300,
         // 轮询频率，每秒1000次
-        poll:1000
+        poll: 1000
     },
-    mode: 'production'
+    mode: 'development',
+    plugins: [
+        // new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        hotOnly: true,
+        // hot: true,
+        host: '172.24.2.4'
+    }
 }
