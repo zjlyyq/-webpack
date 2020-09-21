c27a3cdc3700dd49af7b02dfa40be49920e69f41
tk_zjl <016760@zjtlcb.com>
2020-09-21 21:42:51 +0800

    "30 | Scope Hoisting使用和原理分析"

--- 最简单的webpack/src/index/index.js
+++ 最简单的webpack/src/index/index.js
@@ -1,7 +1,5 @@
 import { helloworld } from '../helloworld'
-import  '../../common/index.js'
 import { common } from '../../common/index.js'
-import '../../common/tree_shaking.js'
 import { a } from '../../common/tree_shaking.js'
 
 document.write(helloworld() + common() + a())
