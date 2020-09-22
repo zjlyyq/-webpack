64a2ce5a9e37d1490afb7a3a1f58b6a0742ce806
tk_zjl <016760@zjtlcb.com>
2020-09-22 17:39:28 +0800

    webpack打包组件和基础库-使用includes设置之针对min.js压缩

--- price-number/dist/price-number.js
+++ price-number/dist/price-number.js
@@ -1,1 +1,158 @@
-!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.priceNumber=t():e.priceNumber=t()}(window,(function(){return function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}return n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){"use strict";n.r(t),t.default={sub:function(e,t){let n=0,r=e.split(".")[1],o=t.split(".")[1],u=e.split(".")[0],l=t.split(".")[0];null==r&&(r="00"),null==o&&(o="00"),r.length<2&&(r+="0"),o.length<2&&(o+="0");let p="",f=1;for(;f>=0;)p=(parseInt(r[f])+n>=parseInt(o[f])?parseInt(r[f])+n-parseInt(o[f]):parseInt(r[f])+n+10-parseInt(o[f]))+p,n=parseInt(r[f])+n>=parseInt(o[f])?0:-1,f-=1;for(p="."+p,f=u.length-l.length;f--;)l="0"+l;for(f=u.length-1;f>=0;)p=(parseInt(u[f])+n>=parseInt(l[f])?parseInt(u[f])+n-parseInt(l[f]):parseInt(u[f])+n+10-parseInt(l[f]))+p,n=parseInt(u[f])+n>=parseInt(l[f])?0:-1,f-=1;let i=0;for(let e=0;e<p.length-2;e++)"0"==p[e]&&"."!=p[e+1]&&(i+=1);return p=p.substring(i),"."==p[0]?"0"+p:p}}}]).default}));
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["priceNumber"] = factory();
+	else
+		root["priceNumber"] = factory();
+})(window, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+function sub(amt1, amt2) {
+    let carry = 0;
+    let p1 = amt1.split('.')[1], p2 = amt2.split(".")[1];
+    let z1 = amt1.split('.')[0], z2 = amt2.split('.')[0];
+    if (p1 == undefined) {
+        p1 = "00";
+    }
+    if (p2 == undefined) {
+        p2 = "00";
+    }
+    if (p1.length < 2) {
+        p1 += "0";
+    }
+    if (p2.length < 2) {
+        p2 += "0";
+    }
+    let ans = "";
+    let c = 1;
+    while (c >= 0) {
+        ans = (parseInt(p1[c]) + carry >= parseInt(p2[c]) ? parseInt(p1[c]) + carry - parseInt(p2[c]) :
+            parseInt(p1[c]) + carry + 10 - parseInt(p2[c])) + ans;
+        carry = parseInt(p1[c]) + carry >= parseInt(p2[c]) ? 0 : -1;
+        c -= 1;
+    }
+    ans = "." + ans;
+    c = z1.length - z2.length;
+    while (c--) {
+        z2 = '0' + z2;
+    }
+    c = z1.length - 1;
+    while (c >= 0) {
+        ans = (parseInt(z1[c]) + carry >= parseInt(z2[c]) ? parseInt(z1[c]) + carry - parseInt(z2[c]) :
+            parseInt(z1[c]) + carry + 10 - parseInt(z2[c])) + ans;
+        carry = parseInt(z1[c]) + carry >= parseInt(z2[c]) ? 0 : -1;
+        c -= 1;
+    }
+    let offset = 0;
+    for (let i = 0; i < ans.length - 2; i++) {
+        if (ans[i] == '0' && ans[i + 1] != '.') {
+            offset += 1;
+        }
+    }
+    ans = ans.substring(offset);
+    return ans[0] == '.' ? "0" + ans : ans;
+}
+
+// console.log(sub("0.17", "0.12"));
+/* harmony default export */ __webpack_exports__["default"] = ({
+    sub
+});
+
+
+
+/***/ })
+/******/ ])["default"];
+});
--- price-number/webpack.config.js
+++ price-number/webpack.config.js
@@ -1,13 +1,22 @@
+const TerserPlugin = require('terser-webpack-plugin')
 module.exports = {
-	mode: "production",
-	entry: {
-		"price-number": "./src/index.js",
-		"price-number.min": "./src/index.js"
-	},
-	output: {
-		filename: "[name].js",
-		library: "priceNumber",    // 指定库的全局变量
-		libraryExport: "default",  
-		libraryTarget: "umd"       // ⽀持库引⼊的⽅式
-	}
+    mode: "none",
+    entry: {
+        "price-number": "./src/index.js",
+        "price-number.min": "./src/index.js"
+    },
+    output: {
+        filename: "[name].js",
+        library: "priceNumber",    // 指定库的全局变量
+        libraryExport: "default",
+        libraryTarget: "umd"       // ⽀持库引⼊的⽅式
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                include: /\.min\.js$/,
+            }),
+        ],
+    }
 }
